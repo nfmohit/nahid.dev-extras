0b6c07151d1dc4d8a80795b8d4bb69767bf2d8a1
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-05-11 08:59:35 +0600

    Added main plugin file

---
+++ nahid.dev-extras.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: nahid.dev Extras
+ * Plugin URI: https://nahid.dev/project/nahid.dev-extras
+ * Description: Gatsby + WordPress extras for the nahid.dev site.
+ * Author: Nahid Ferdous Mohit
+ * Version: 1.0
+ * Author URI: https://nahid.dev
+ * Text Domain: nahid.dev-extras
+ *
+ * @since      1.0
+ * @package    nahid.dev Extras
+ * @author     Nahid Ferdous Mohit
+ */
+/*
+ * If this file is called directly, abort.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
