f2057128e90c7ae01fa5c896463c65ba53154219
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-06-09 21:35:12 +0600

    Added the field and made it work

--- includes/metaboxes/project-usage.php
+++ includes/metaboxes/project-usage.php
@@ -29,5 +29,41 @@ function project_usage_metabox() {
 add_action( 'add_meta_boxes', 'project_usage_metabox' );
 
 function project_usage_metabox_callback() {
-	echo 'Hello World';
+
+	global $post;
+
+    $values = get_post_custom( $post->ID );
+
+    $usage_text = isset( $values[ 'project_usage_metabox_usage_text' ] ) ? esc_html( $values[ 'project_usage_metabox_usage_text' ][ 0 ] ) : '';
+
+    wp_nonce_field( 'project_usage_metabox_nonce', 'meta_box_nonce' );
+
+    ?>
+
+    <div class="components-base-control">
+        <div class="components-base-control__field">
+            <label class="components-base-control__label" for="project_usage_metabox_usage_text"><?php echo __( 'Project Usage Text', 'nahid.dev-extras' ); ?></label>
+            <textarea class="components-textarea-control__input" id="project_usage_metabox_usage_text" name="project_usage_metabox_usage_text"><?php echo esc_html( $usage_text ); ?></textarea>
+        </div>
+    </div>
+
+    <?php
 }
+
+function project_usage_metabox_save( $post_id ) {
+
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+
+    if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'project_usage_metabox_nonce' ) ) return;
+
+    if( !current_user_can( 'edit_post' ) ) return;
+
+    if( isset( $_POST['project_usage_metabox_usage_text'] ) ) {
+        if ( $_POST['project_usage_metabox_usage_text'] == '' ) {
+            delete_post_meta( $post_id, 'project_usage_metabox_usage_text' );
+        } else {
+            update_post_meta( $post_id, 'project_usage_metabox_usage_text', esc_html( $_POST['project_usage_metabox_usage_text'] ) );
+        }
+    }
+}
+add_action( 'save_post', 'project_usage_metabox_save' );
